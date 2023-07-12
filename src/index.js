a6c1fe382d89203870cdedb495439c08d2d59a88
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2023-07-12 14:18:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Emitter from "./classes/Emitter.js";
 import Checker from "./classes/Checker.js";
-import Error from "./classes/Error.js"
 
-export { Emitter, Checker, Error };
+export { Emitter, Checker };
